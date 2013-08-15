4caf4902b2156882a88814b019152a40a9f040fd
digital-flowers <digital.flowers@hotmail.com>
2013-08-15 20:08:08 +0300

    test server

---
+++ index.js
@@ -0,0 +1,6 @@
+var http = require("http");
+http.createServer(function(request,response){
+    response.writeHead(200,"text/plain");
+    response.write("LOL");
+    response.end();
+}).listen(8000);
