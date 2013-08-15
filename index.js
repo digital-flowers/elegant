b20c1a679663d1121a28fe4496fff29553d7a881
hassan alnator <hassanalnator@gmail.com>
2013-08-15 21:06:25 +0300

    Update index.js
    ss

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 http.createServer(function(request,response){
     response.writeHead(200,"text/plain");
-    response.write("LOL");
+    response.write("LOLs");
     response.end();
 }).listen(8000);
