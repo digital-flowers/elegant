4dc729d4bdf953f93f04aaec054ad49e7213032d
digital-flowers <digital.flowers@hotmail.com>
2013-08-16 18:39:54 +0300

    test server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require("http");
-http.createServer(function(request,response){
-    response.writeHead(200,"text/plain");
-    response.write("LOL");
+var web = require("./modules/web.js");
+http.createServer(function (request, response) {
+    var req = web.parseRequest(request);
+    response.writeHead(req.code, "text/plain");
+    response.write(req.params["name"]+"");
     response.end();
 }).listen(8000);
---
+++ modules/web.js
@@ -0,0 +1,37 @@
+var qs = require('querystring');
+var url = require("url");
+var settings = require("../settings");
+var extend = require("../node_modules/extend");
+
+exports.parseRequest = function (request) {
+    var data = {
+        method: request.method,
+        path: "",
+        params: {},
+        args: [],
+        code: 200,
+        success: false
+    };
+    // parse url and GET data
+    if (request.url) {
+        var urlData = url.parse(request.url, true);
+        data.path = urlData.pathname;
+        data.args = data.path.split("/").slice(1);
+        extend(data.params, urlData.query);
+    }
+    // parse POST data
+    var requestBody = '';
+    request.on('data', function (data) {
+        requestBody += data;
+        if (requestBody.length > 1e6 * settings.POST_MAX_SIZE) {
+            data.success = false;
+            data.code = 413;
+            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+            request.connection.destroy();
+        }
+    });
+    request.on('end', function () {
+        extend(data.params, qs.parse(requestBody));
+    });
+    return data;
+};
