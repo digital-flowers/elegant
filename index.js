f9fdf53e1519318a93093b43f9a38afed0794b38
digital-flowers <digital.flowers@hotmail.com>
2013-08-18 21:59:46 +0300

    test server

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ var http = require("http");
 var web = require("./modules/web.js");
 http.createServer(function (request, response) {
     var req = web.parseRequest(request);
+    // TODO: hasan template engine
+    /*
+    var vars = {
+        user: {
+            name:"fareed",
+            age:25
+        },
+        product:{
+            name:"BMW",
+            year:2010
+        }
+    }
+    var result = theme("product.html",vars);
     response.writeHead(req.code, "text/plain");
-    response.write(req.params["name"]+"");
+    response.write(result+"");
+    */
+    response.writeHead(req.code, "text/plain");
+    response.write("LOL");
     response.end();
 }).listen(8000);
