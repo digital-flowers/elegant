e6921e13ed960ec2896cc4c11a8acd8dcb4a4538
hasan <hassanalnator@gmail.com>
2013-08-19 08:58:52 +0300

    Update Theme Engine

--- modules/theme.js
+++ modules/theme.js
@@ -5,9 +5,30 @@
  * Time: 10:10 PM
  * To change this template use File | Settings | File Templates.
  */
+var settings = require("../settings");
+var fs = require("fs");
 
-exports.render = function (request) {
+exports.RenderView = function (view,vars) {
+
+   // Get Defult Folder From Settings
+   var ViewDR = "" + settings['VIEWS_DIR'];
+
+   // Construct View Path
+   var ViewPath = "views/product.html";
+
+   // Read View File
+
+   var file = fs.readFile(ViewPath, 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+
+              return console.log(data);
 
 
+
+    });
+
+    return file;
 
 };
