29113c7b8b94f2f5203495d3e3e45cd1bb0c2cd6
hasan <hassanalnator@gmail.com>
2013-08-23 18:17:34 +0300

    Update Theme Engine

--- controllers/about.js
+++ controllers/about.js
@@ -2,12 +2,16 @@
 
 var settings = require("../settings.js");
 var view = require("../modules/theme.js");
+var db = require("../modules/db.js");
 
 
 exports["/about"] = {
     handler:function(data){
         // Prepare Vars
-        var vars = {article: { pagename: 'Swig is fun!' ,writer:['hassan','fareed']}};
+
+
+        var Query  = db("SELECT * FROM `auth_user`");
+        var vars = {article: { pagename: 'Swig is fun!' ,writer:["fareed","hassan"]}};
 
         //Render View with its Variables
         return view('about.html',vars);
